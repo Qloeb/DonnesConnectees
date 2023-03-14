d8ab2648c1cdf9aea71b8faa1f5745f5c0e46ec8
Quentin Loeb <quentin.loeb@gmail.com>
2023-03-14 09:32:49 +0100

    Ajout réception requête POST côté serveur

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
 //localhost:3000 pour voir les ressources
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/toto", function(request, response){
     response.send("salut toto");
@@ -13,3 +17,9 @@ app.get("/toto", function(request, response){
 app.listen(port, function(){
 	console.log('serveur listening on port : '+port);
 });
+
+app.post("/donnees", function(req, res) {
+    var donnees = req.body;
+    console.log(donnees);
+    res.send("Données reçues !");
+  });
