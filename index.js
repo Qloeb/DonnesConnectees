8b511cb78bc718131a60b656d1f88068f58a6593
Quentin Loeb <quentin.loeb@gmail.com>
2023-03-15 09:56:13 +0100

    GET toutes annotations d'une ressource

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var data=[];
+var id = 0;
+var data={};
+
 
 app.use(express.static('html'));
 
@@ -14,13 +16,27 @@ app.use(express.static('html'));
 app.post("/annotation", function(req, res){
 	var body = req.body;
 	console.log(body);
-	data.push(body);
+	data[id] = body;
+	id++;
 	console.log(data);
 	res.send();
 });
 
 app.get("/recup_annotations", function(req, res){
 		res.send(data);
+});
+
+app.get("/recup_annotations_uri", function(req, res){
+	var uri = req.query.uri;
+	console.log(uri);
+	tab_annot = [];
+	for (idx in data){
+		if (data[idx]["URI"] == uri){
+			tab_annot.push(data[idx]["Commentaire"]);
+		}
+	}
+	console.log(tab_annot);
+	res.send(tab_annot);
 });
 	
 
